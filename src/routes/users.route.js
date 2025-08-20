178fbae435b23ba15a20e52627b6fb458fe8f6fc
Allieniaszxy <alakaalamin40@gmail.com>
2025-08-20 06:08:39 -1100

    added some models and controllers

--- src/routes/users.route.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { signUpUsers, signInUsers } = require("../controllers/auth.controller");
-const validate = require("../middlewares/validate");
-const { signupSchema, signinSchema } = require("../validators/auth.schema");
-
-router.post("/signup", validate(signupSchema), signUpUsers);
-router.post("/signin", validate(signinSchema), signInUsers);
-
-module.exports = router;
